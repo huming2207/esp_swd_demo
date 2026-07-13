45056a4deaf1cd7ab847aa5c22f66885e0222842
Jackson Ming Hu <huming2207@gmail.com>
2026-07-13 11:03:12 +1000

    Add rev6 demo

--- main/demo_main.c
+++ main/demo_main.c
@@ -1,25 +1,26 @@
-#include <stdio.h>
-#include <string.h>
-#include <swd_host.h>
+#include <inttypes.h>
+
 #include <esp_log.h>
-#include <esp_timer.h>
+#include <swd_host.h>
 
 void app_main(void)
 {
     static const char *TAG = "main";
-    ESP_LOGI(TAG, "Lay hou from DAPLink!");
+    ESP_LOGI(TAG, "Soul Injector Rev 6 SWD GPIO PoC");
 
-    swd_init_debug();
+    if (!swd_init_debug()) {
+        ESP_LOGE(TAG, "SWD initialization failed");
+        swd_off();
+        return;
+    }
 
     uint32_t idcode = 0;
-    uint8_t ret = swd_read_idcode(&idcode);
+    if (!swd_read_idcode(&idcode)) {
+        ESP_LOGE(TAG, "DP IDCODE read failed");
+        swd_off();
+        return;
+    }
 
-    ESP_LOGI(TAG, "IDCode readout is: 0x%x, ret: %u", idcode, ret);
-
-    uint8_t *buf = malloc(8192);
-    memset(buf, 0x5a, 8192);
-
-    int64_t start_ts = esp_timer_get_time();
-    ret = swd_write_memory(0x20000000, buf, 8192);
-    ESP_LOGI(TAG, "Wrote 8KB used %lld us, ret %u", esp_timer_get_time() - start_ts, ret);
+    ESP_LOGI(TAG, "DP IDCODE: 0x%08" PRIx32, idcode);
+    swd_off();
 }
